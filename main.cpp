0313b93a43d019cc24b59af9f8926866c1dd25a8
Lopuh <1opuhart@gmail.com>
2023-06-10 19:25:40 +0900

    fixes

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "headers/books.h"
 #include "headers/user.h"
 
-void action();
+bool action();
 
 int main(){
     std::string login; int password;
@@ -11,21 +11,27 @@ int main(){
     password = get_password();
     }
     while (!check_login(login, password));
-    action();
+    
+    while (action()){
+        action();
+    }
+
     return 0;
 }
 
-void action(){
+bool action(){
     int choice;
-    std::cout << "What you're going to do?(1 - create book, 2 - list all books)" << std::endl;
+    std::cout << "What are you going to do?(create book(1), list all books(2), exit(3))" << std::endl;
     std::cin >> choice;
     switch (choice){
         case 1: 
             create_book(); break;
         case 2: 
             list_books(); break;
+        case 3:
+            return false; 
         default:
             std::cout << "There's not that option";
     }
-
+    return true;
 }
