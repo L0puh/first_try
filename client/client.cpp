de69b30aee6939f50fcac55670c285a877f22381
Lopuh <1opuhart@gmail.com>
2023-06-28 21:19:45 +0900

    threads
    ability to send messages from multiply clients

--- client/client.cpp
+++ client/client.cpp
@@ -2,44 +2,39 @@
 #include <WinSock2.h>
 #include <iostream>
 #pragma warning(disable:4996)
- 
-int print_error(int result);
-void print_message(SOCKET Connection);
+
+void send_message(SOCKET Connection);
 
 int main(int argc, char* argv[]) {
+	
 	WSAData wsaData;
-	WORD DllVersion = MAKEWORD(2, 1); // choose version to winsock
-	int res = WSAStartup(DllVersion, &wsaData);  // start the lib
-	print_error(res);
+	WORD DllVersion = MAKEWORD(2, 1); 
+	int res = WSAStartup(DllVersion, &wsaData);  
 
-	SOCKADDR_IN addr; // storage for the address 
+
+	SOCKADDR_IN addr; 
 	int sizeofaddr = sizeof(addr);
-	addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // ip address
-	addr.sin_port = htons(1111); // port for identification the programm 
-	addr.sin_family = AF_INET; // family of internet protocols
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+	addr.sin_port = htons(1111); 
+	addr.sin_family = AF_INET; 
 
 	SOCKET Connection = socket(AF_INET, SOCK_STREAM, NULL);
 	res = connect(Connection, (SOCKADDR*)&addr, sizeofaddr);
-	
-	if (res == 0) 
-		std::cout << "connected to the server\n";
-	else 
-		print_error(res);
 
-	
-	print_message(Connection);
+	if (res == 0)
+		std::cout << "connected to the server\n";
+
+	while(true)
+		send_message(Connection);
 
 	return 0;
 }
 
-int print_error(int result) {
-	if (result != 0) {
-		std::cout << "error in client" << std::endl;
-		return 1;
-	}
-}
-void print_message(SOCKET Connection) {
-	char msg[200];
-	recv(Connection, msg, sizeof(msg), NULL);
-	std::cout << "\nmessage from the server: " << msg << std::endl;
+
+void send_message(SOCKET Connection) {
+	char message[256];
+	std::cout << "enter a message: \n>";
+	std::cin.getline(message, sizeof(message));
+	send(Connection, message, sizeof(message), NULL);
+	
 }
--- server/server.cpp
+++ server/server.cpp
@@ -3,41 +3,45 @@
 #include <iostream>
 #pragma warning(disable:4996)
 
+SOCKET Connection;
+SOCKET Connections[3];
+
+void receive_messages(SOCKET Connection);
+
 int main(int argc, char* argv[]) {
 	WSAData wsaData;
-	WORD DllVersion = MAKEWORD(2, 1); // choose a version of winsock
-	if (WSAStartup(DllVersion, &wsaData) != 0) { // start the lib
-		std::cout << "error" << std::endl;
-		return -1;
-	}
-	SOCKADDR_IN addr; // storage for the address 
+	WORD DllVersion = MAKEWORD(2, 1); 
+	WSAStartup(DllVersion, &wsaData);
+	
+	SOCKADDR_IN addr;
 	int sizeofaddr = sizeof(addr);
-	addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // ip address
-	addr.sin_port = htons(1111); // port for identification the programm 
-	addr.sin_family = AF_INET; // family of internet protocols
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+	addr.sin_port = htons(1111); 
+	addr.sin_family = AF_INET; 
 
-	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL); // create the socket
-
-	// link the address to the socket
-	// get the socket, the link to address and size
+	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL); 
 	bind(sListen, (SOCKADDR*)&addr, sizeof(addr));
+	listen(sListen, SOMAXCONN); 
+	
+	
+	for (int i = 0; i < 3; i++) {
 
-	listen(sListen, SOMAXCONN); // start listening, 
-	// gets the socket and max limit of requests
-
-	SOCKET newConnection;
-	// keep data of connection 
-	newConnection = accept(sListen, (SOCKADDR*)&addr, &sizeofaddr); 
-
-	if (newConnection == 0) {
-		std::cout << "error in connection\n";
+		Connection = accept(sListen, (SOCKADDR*)&addr, &sizeofaddr);
+		if (Connection != 0) {
+			std::cout << "connection is success\n";
+			Connections[Connection];
+			CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)receive_messages, (LPVOID)(Connection), NULL, NULL);
+		}
 	}
-	else {
-		std::cout << "connection is success\n";
-		char msg[] = "hello world";
-		send(newConnection, msg, sizeof(msg), NULL);
-	}
-	 
-
 	return 0;
+}
+
+void receive_messages(SOCKET Connection)
+{
+	while (true) {
+		char msg[256];
+		int bytes = recv(Connection, msg, sizeof(msg), NULL);
+		if (bytes > 0)
+			std::cout << msg << std::endl;
+	}
 }
