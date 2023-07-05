075f1008d3c5eb716b1829ba4da07717f7f0f84a
Lopuh <1opuhart@gmail.com>
2023-07-05 17:45:57 +0900

    modified server, need fixes

--- networking/server_exmp/server.cpp
+++ networking/server_exmp/server.cpp
@@ -1,6 +1,11 @@
 #include "headers/net.h"
-#include <sys/socket.h>
-
+#include <vector>
+struct connection_t {
+    int index;
+    int socketfd;
+};
+std::vector<connection_t> connections;
+int current_socket;
 int create_socket(struct addrinfo *servinfo, struct addrinfo *p, int sockfd){
     int res, yes = 1;
     for (p = servinfo; p != NULL; p = p->ai_next){
@@ -52,41 +57,50 @@ int main () {
     socklen_t sin_size;
     struct sockaddr_in their_addr;
     char s[INET_ADDRSTRLEN];
-    while (true){
+    for (int i = 0; i != BACKLOG; i ++){
         sin_size = sizeof(their_addr);
         int sockfd_new = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
         if (print_error(sockfd_new)){
             continue;
         }
+        connection_t conn;
+        conn.index = i; conn.socketfd = sockfd_new;
+        connections.push_back(conn);
+
         inet_ntop(their_addr.sin_family, 
                 get_in_addr((struct sockaddr *)&their_addr), s, sizeof(s));
         printf("connection from: %s\n", s);
-
+        
         pid_t thread = fork();
         if(!thread){
-            close(sockfd);
-
-            int msg_size;
-            while(true) {
-            recv(sockfd_new, (char*)&msg_size, sizeof(int), 0);
-            char* msg = new char[msg_size+1];
-            msg[msg_size] = '\0';
             
-            int bytes_sent = recv(sockfd_new, msg, msg_size, 0);
-            if (bytes_sent > 0 ){
-                printf("msg from client: %s (size: %d bytes)\n", msg, msg_size);
-            } else {
-                print_error(bytes_sent);
-                close(sockfd_new);
-                exit(1);
-            }
-             
-            delete[] msg; 
+            current_socket = i;
+            int msg_size;
+            while (true) {
 
+                //FIXME: add async receiving 
+                for(int i = connections.size()-1; i != -1; i--) {
+
+                    recv(connections.at(i).socketfd, (char*)&msg_size, sizeof(int), 0);
+                    char* msg = new char[msg_size+1];
+                    msg[msg_size] = '\0';
+                    
+                    int bytes_sent = recv(connections.at(i).socketfd, msg, msg_size, 0);
+                    if (bytes_sent > 0 ){
+                        printf("msg from client(%d): %s (size: %d bytes)\n", i, msg, msg_size);
+                    }
+                    else {
+                        print_error(bytes_sent);
+                        close(sockfd_new);
+                        exit(1);
+                    }
+                     
+                delete[] msg; 
+            
+                }
             }
         }
-    }
-
+    } 
     return 0;
 }
 
