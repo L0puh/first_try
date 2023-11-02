3adbda046e7da5d1037bd9a186e52e4f311df4e9
L0puh <1opuhart@gmail.com>
2023-11-02 21:01:58 +0900

    lib

---
+++ gui/using_imgui/src/main.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+
+int main(int argc, char** argv) {
+    if (!glfwInit()) 
+        return 1;
+    
+    GLFWwindow* window = glfwCreateWindow(300, 300, "test window", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init();
+
+     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    while(!glfwWindowShouldClose(window)){
+        glfwPollEvents();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        ImGui::Begin("Hello, world!");
+        ImGui::Text("example text");
+        ImGui::End();
+        
+        ImGui::Render();
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+    
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
