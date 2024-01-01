265485e6a0bb43d68fe2191a379ad729ad7c5c41
L0puh <1opuhart@gmail.com>
2024-01-01 21:29:43 +0900

    min cost path

--- algorithms/knapsack.cpp
+++ algorithms/knapsack.cpp
@@ -1,52 +1,37 @@
-#include <algorithm>
 #include <iostream>
-#include <vector>
-
 
 struct item {
     int weight;
     int value;
 };
 
-int main() {
-    std::vector<item> knapsack;
-    int w, v, max;
-    std::cin >> max;
-    for (int i = 0; i!=3; i++){
-        std::cin >>  w >> v;
-        knapsack.push_back({w, v});
-    }
-    int grid[knapsack.size()+1][max+1];
 
-    for (int i = 0; i <= knapsack.size(); i++){
+
+int main() {
+    int w, v, max, size, mix;
+    item knapsack[size];
+    std::cin >> max >> size;
+
+    for (int i = 0; i != size; i++){
+        std::cin >>  w >> v;
+        knapsack[i] = {w, v};
+    }
+
+    int grid[size+1][max+1];
+    for (int i = 0; i <= size; i++){
         for (int j = 0; j <= max; j++){
-            if (i == 0 or j == 0) grid[i][j] = 0;
+            if (i == 0 || j == 0) 
+                grid[i][j] = 0;
+
             else if (knapsack[i-1].weight <= j){
-                grid[i][j] = std::max(knapsack.at(i-1).value + grid[i-1][j - knapsack.at(i-1).weight],
-                                     grid[i-1][j]);
+                mix = knapsack[i-1].value + grid[i-1][j - knapsack[i-1].weight],
+                grid[i][j] = std::max(mix, grid[i-1][j]); // maximum of the current item + with item less weight
+                                                          // vs previous item 
             }
             else grid[i][j] = grid[i-1][j];
         }
     }
-    printf("%d\n", grid[knapsack.size()][max]);
+
+    printf("%d\n", grid[size][max]);
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ algorithms/min_cost_path.cpp
@@ -0,0 +1,36 @@
+/*
+min cost path (DP)
+*/
+
+#include <bits/c++io.h>
+
+int square[4][4] = {
+    {4, 3, 7},
+    {9, 2, 5},
+    {1, 2, 6},
+};
+
+int main(){
+    int rows = 3, cols = 3;
+    int ans[4][4];
+    ans[0][0] = square[0][0];
+    for (int i = 1; i != rows; i++ ){
+        ans[i][0] = ans[i-1][0] + square[i][0];
+    }
+    for (int i = 1; i != cols; i++ ){
+        ans[0][i] = ans[0][i-1] + square[0][i];
+    }
+    for (int i = 1; i != rows; i++ ){
+        for (int j = 1; j != cols; j++){
+            if (ans[i][j-1] + square[i][j] > ans[i-1][j] + square[i][j]){
+                ans[i][j] = square[i][j] + ans[i-1][j];
+            } else 
+                ans[i][j] = square[i][j] + ans[i][j-1];
+        }
+    }
+    
+    printf("%d\n", ans[rows-1][cols-1]); 
+
+    return 0;
+}
+        
